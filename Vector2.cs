0d6f4200b3b1d0331b0e683cd871c0f642860839
thebotfly <69916612+thebotfly@users.noreply.github.com>
2020-10-14 15:37:18 -0500

    Part of the solution

---
+++ Vector2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Math_Library
+{
+    public class Vector2
+    {
+        private float _x;
+        private float _y;
+
+        public Vector2()
+        {
+            _x = 0;
+            _y = 0;
+        }
+
+        public Vector2(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+
+    }
+}
