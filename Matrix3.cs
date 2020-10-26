dc20127439b879a1fce03e648728c9e077bcd979
thebotfly <69916612+thebotfly@users.noreply.github.com>
2020-10-26 17:00:35 -0500

    Added new classes

---
+++ Matrix3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_Library
+{
+    public class Matrix3
+    {
+        public float m11, m12, m13, m21, m22, m23, m31, m32, m33;
+
+        public Matrix3()
+        {
+            m11 = 1; m12 = 0; m13 = 0;
+            m21 = 0; m22 = 1; m23 = 0;
+            m31 = 0; m32 = 0; m33 = 1;
+
+        }
+
+        public Matrix3(float m11, float m12, float m13, float m21, float m22, float m23, float m31, float m32, float m33)
+        {
+            this.m11 = m11; this.m12 = m12; this.m13 = m13;
+            this.m21 = m21; this.m22 = m22; this.m23 = m23;
+            this.m31 = m31; this.m32 = m32; this.m33 = m33;
+        }
+
+        public static Matrix3 operator +(Matrix3 Ihs, Matrix3 rhs)
+        {
+            return new Matrix3(Ihs.m11 + rhs.m11, Ihs.m12 + rhs.m12, Ihs.m13 + rhs.m13, Ihs.m21 + rhs.m21, Ihs.m22 + rhs.m22, Ihs.m23 + rhs.m23, Ihs.m31 + rhs.m31, Ihs.m32 + rhs.m32, Ihs.m33 + rhs.m33);
+        }
+        public static Matrix3 operator -(Matrix3 Ihs, Matrix3 rhs)
+        {
+            return new Matrix3(Ihs.m11 - rhs.m11, Ihs.m12 - rhs.m12, Ihs.m13 - rhs.m13, Ihs.m21 - rhs.m21, Ihs.m22 - rhs.m22, Ihs.m23 - rhs.m23, Ihs.m31 - rhs.m31, Ihs.m32 - rhs.m32, Ihs.m33 - rhs.m33);
+        }
+
+        public static Matrix3 operator *(Matrix3 Ihs, Matrix3 rhs)
+        {
+            return new Matrix3(
+                Ihs.m11 * rhs.m11 + Ihs.m12 * rhs.m21 + Ihs.m13 * rhs.m31,
+                + Ihs.m11 * rhs.m12 + Ihs.m12 * rhs.m22 + Ihs.m13 * rhs.m32,
+                + Ihs.m11 * rhs.m13 + Ihs.m12 * rhs.m23 + Ihs.m13 * rhs.m33,
+               Ihs.m21 * rhs.m11 + Ihs.m22 * rhs.m21 + Ihs.m23 * rhs.m31,
+                +Ihs.m21 * rhs.m12 + Ihs.m22 * rhs.m22 + Ihs.m23 * rhs.m32,
+                +Ihs.m21 * rhs.m13 + Ihs.m22 * rhs.m23 + Ihs.m23 * rhs.m33,
+                Ihs.m31 * rhs.m11 + Ihs.m32 * rhs.m21 + Ihs.m33 * rhs.m31,
+                +Ihs.m31 * rhs.m12 + Ihs.m32 * rhs.m22 + Ihs.m33 * rhs.m32,
+                +Ihs.m31 * rhs.m13 + Ihs.m32 * rhs.m23 + Ihs.m33 * rhs.m33);
+        }
+
+    }
+}
---
+++ Vector3.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_Library
+{
+    class Vector3
+    {
+        private float _x;
+        private float _y;
+
+        public Vector3()
+        {
+            _x = 0;
+            _y = 0;
+        }
+
+        public Vector3(float x, float y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+
+        }
+
+        public float Magnitude
+        {
+            get
+            {
+                return (float)Math.Sqrt(X * X + Y * Y);
+            }
+        }
+
+        public Vector3 Normalized
+        {
+            get
+            {
+                return Normalize(this);
+            }
+        }
+
+        public static Vector3 Normalize(Vector3 vector)
+        {
+            if (vector.Magnitude == 0)
+                return new Vector3();
+            return vector / vector.Magnitude;
+        }
+
+
+        public static Vector3 operator +(Vector3 Ihs, Vector3 rhs)
+        {
+            float x = Ihs.X + rhs.X;
+            float y = Ihs.Y + rhs.Y;
+
+            return new Vector3(x, y);
+        }
+
+        public static Vector3 operator -(Vector3 Ihs, Vector3 rhs)
+        {
+            return new Vector3(Ihs.X - rhs.X, Ihs.Y - rhs.Y);
+        }
+        public static Vector3 operator *(Vector3 Ihs, float scalar)
+        {
+            return new Vector3(Ihs.X * scalar, Ihs.Y * scalar);
+        }
+
+        public static Vector3 operator /(Vector3 Ihs, float scalar)
+        {
+            return new Vector3(Ihs.X / scalar, Ihs.Y / scalar);
+        }
+
+        public static Vector3 DotProduct(Vector3 Ihs, Vector3 rhs)
+        {
+            return (Ihs.X * rhs.X, +Ihs.Y * rhs.Y);
+        }
+
+    }
+}
